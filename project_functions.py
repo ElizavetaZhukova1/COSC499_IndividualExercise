19ddc0b0a972a9818c3de8951e37ca285a69aeb1
ElizavetaZhukova1 <elizavetazhukova57@gmail.com>
2022-09-14 19:58:08 -0700

    Added the second feature for avg num of words per sent

--- project_functions.py
+++ project_functions.py
@@ -8,6 +8,15 @@ def calc_sym_words_ratio(text):
     if words_num<1:
         return None
     return round(sym_count/words_num,3)
+def calc_words_sent_ratio(text):
+    sents_arr=re.split(r"[.!?\n]", text)
+    sents_arr=[i for i in sents_arr if i]
+    words=words_tokenize(text)
+    sents_num=len(sents_arr)
+    num_words=len(words)
+    if sents_num<1 or num_words<1:
+        return None
+    return round(num_words/sents_num,3)
 def words_tokenize(u_str):
     prep_text=re.sub(r"[^a-z0-9 ]"," ",u_str.lower()) #substitute all characters other than letters, numbers and whitespaces with whitespaces
     prep_text=re.sub(r" +", " ", prep_text)
